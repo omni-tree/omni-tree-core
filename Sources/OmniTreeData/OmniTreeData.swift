f4a221a3ce116f76e1b117518b4d4f7349829534
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2019-01-13 10:46:55 -0800

    Added copyright notices.

--- Sources/OmniTreeData/OmniTreeData.swift
+++ Sources/OmniTreeData/OmniTreeData.swift
@@ -1,3 +1,5 @@
+// Copyright 2018 The OmniTree Authors.
+
 struct OmniTreeData {
   var text = "Hello, World!"
 }
--- Tests/OmniTreeSchemaTests/OmniTreeSchemaTests.swift
+++ Tests/OmniTreeSchemaTests/OmniTreeSchemaTests.swift
@@ -1,3 +1,5 @@
+// Copyright 2018 The OmniTree Authors.
+
 import XCTest
 @testable import OmniTreeSchema
 
