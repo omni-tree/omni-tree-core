be625e6e9da1a15e0e1721cb4e6e12462f639c50
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2019-05-05 14:58:25 -0700

    Convert all classes in ImmutableSchema into protocols.

--- Sources/OmniTreeSchema/ImmutableSchema.swift
+++ Sources/OmniTreeSchema/ImmutableSchema.swift
@@ -1,8 +1,8 @@
 // Copyright 2019 The OmniTree Authors.
 
-// Immutable protocols and classes for elements of the OmniTree schema.
+// Immutable protocols for elements of the OmniTree schema.
 
-/// This is the base class for all schema elements. It contains fields that
+/// This is the base protocol for all schema elements. It contains fields that
 /// are common to all schema elements.
 public protocol ElementSchema {
   var name: String { get }
@@ -69,72 +69,63 @@ public protocol EntityFieldSchema: FieldSchema {
 // MARK: - Predefined Primitives -
 
 /// Schema for primitives.
-/// There are subclasses for each type of primitive.
+/// There are sub-protocols for each type of primitive.
 public protocol PrimitiveSchema {}
 
 /// Schema for boolean primitive.
-public class BooleanSchema: PrimitiveSchema {}
+public protocol BooleanSchema: PrimitiveSchema {}
 
 /// Schema for numeric primitives.
-public class NumericSchema<T: Numeric>: PrimitiveSchema {
-  var constraints: NumericConstraints<T>
+public protocol NumericSchema: PrimitiveSchema {
+  associatedtype T: NumericConstraints
 
-  init(constraints: NumericConstraints<T>) {
-    self.constraints = constraints
-  }
+  var constraints: T { get }
 }
 
 /// Schema for string primitive.
-public class StringSchema: PrimitiveSchema {
-  var constraints: StringConstraints
-
-  init(constraints: StringConstraints) {
-    self.constraints = constraints
-  }
+public protocol StringSchema: PrimitiveSchema {
+  var constraints: StringConstraints { get }
 }
 
 /// Schema for password primitive that can be encrypted but not decrypted
 /// (1-way).
-public class Password1WaySchema: StringSchema {}
+public protocol Password1WaySchema: StringSchema {}
 
 /// Schema for password primitive that can be encrypted as well as decrypted
 /// (2-way).
-public class Password2WaySchema: StringSchema {}
+public protocol Password2WaySchema: StringSchema {}
 
 /// Schema for UUID fields.
-public class UuidSchema: PrimitiveSchema {}
+public protocol UuidSchema: PrimitiveSchema {}
 
 /// Schema for blob primtive that can store binary data.
-public class BlobSchema: PrimitiveSchema {}
+public protocol BlobSchema: PrimitiveSchema {}
 
 // MARK: - Constraints -
 
 /// Constraints on the cardinality of a field.
-public class Multiplicity {
-  let min: UInt = 1
-  let max: UInt = 1
+public protocol Multiplicity {
+  var min: UInt { get }
+  var max: UInt { get }
 }
 
 /// Constraints for numeric fields.
-public class NumericConstraints<T> {
-  let minValue: NumericBound<T>? = nil
-  let maxValue: NumericBound<T>? = nil
+public protocol NumericConstraints {
+  associatedtype T: NumericBound
+  var minValue: T? { get }
+  var maxValue: T? { get }
 }
 
 /// An inclusive/exclusive bound for a numeric field.
-public class NumericBound<T> {
-  let value: T
-  let inclusive: Bool
-
-  init(value: T, inclusive: Bool = false) {
-    self.value = value
-    self.inclusive = inclusive
-  }
+public protocol NumericBound {
+  associatedtype T: Numeric
+  var value: T { get }
+  var inclusive: Bool { get }
 }
 
 /// Constraints for string fields.
-public class StringConstraints {
-  let minLength: Int? = nil
-  let maxLength: Int? = nil
-  let regexPattern: String? = nil
+public protocol StringConstraints {
+  var minLength: Int? { get }
+  var maxLength: Int? { get }
+  var regexPattern: String? { get }
 }
